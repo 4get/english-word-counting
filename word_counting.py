4bb3dfd23ed090de2926ad46ed261c42d4b7f02e
Yaowu Yang <iyaowu@gmail.com>
2012-11-12 10:04:11 +0800

    remove easy word

--- word_counting.py
+++ word_counting.py
@@ -9,8 +9,13 @@ for filename in sys.argv[1:]:
 	words_gen = (word.strip(punctuation).lower() for line in open(filename)
                                          for word in line.split())
 
+easy_words_gen = (word.strip(punctuation).lower() for line in open("easy_words.txt")
+                                         for word in line.split())
 for word in words_gen:
     words[word] = words.get(word, 0) + 1
+
+for word in easy_words_gen:
+    del words[word]
 
 top_words = sorted(words.items(), key=itemgetter(1), reverse=True)[:N]
 
